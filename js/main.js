c608059e02dff1e14b453dfe05a8c1e06cebdd12
Yura Lee <cong3022@gmail.com>
2025-08-13 20:17:49 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -117,3 +117,12 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+
+function setVh() {
+  document.documentElement.style.setProperty(
+    '--vh',
+    `${window.innerHeight * 0.01}px`
+  );
+}
+setVh();
+window.addEventListener('resize', setVh);
